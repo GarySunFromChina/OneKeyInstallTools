f580ffc3a49879ac3124168a3040482b9ee6bb94
GarySunFromChina <36440190+GarySunFromChina@users.noreply.github.com>
2018-08-24 19:29:39 +0800

    Update setup_wins_centos.sh

--- setup_wins_centos.sh
+++ setup_wins_centos.sh
@@ -5,13 +5,15 @@ systemctl enable smb nmb winbind
 
 #sed -i  '/workgroup/{/wins/!s/$/ wins/}' /etc/nsswitch.conf
 
-sed -i '/workgroup/{/wins/!s/=.*/= $0/p}' /etc/samba/smb.conf 
 
-sed -i '/workgroup/a\        netbios name = $1' /etc/samba/smb.conf 
+
+sed -i '/workgroup/{/wins/!s/=.*/= ${0}/p}' /etc/samba/smb.conf 
+
+sed -i '/workgroup/a\        netbios name = ${1}' /etc/samba/smb.conf 
 
 sed -i '/workgroup/a\        min procotol = SMB2' /etc/samba/smb.conf 
 
-sed -i  '/^hosts:/{/wins/!s/$/ wins/}' /etc/nsswitch.conf
+sed -i  '/^hosts:/{/wins/!s/files /files wins/}' /etc/nsswitch.conf
 
 
 firewall-cmd --permanent --add-service=samba
