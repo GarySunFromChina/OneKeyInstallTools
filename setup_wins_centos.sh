b596810f44ca7b85e98ca273ad54ade9eeee2d16
GarySunFromChina <36440190+GarySunFromChina@users.noreply.github.com>
2018-08-24 20:22:47 +0800

    Update setup_wins_centos.sh

--- setup_wins_centos.sh
+++ setup_wins_centos.sh
@@ -6,6 +6,18 @@ systemctl enable smb nmb winbind
 group=$1
 name=$2
 
+if [$group==""];then
+    echo "please input workgroup[default:WORKGROUP]:"
+    read group
+fi
+
+if [$name==""];then
+    echo "please input hostname[default:CentOS]:"
+    read name
+fi
+
+
+
 sed -i '/workgroup/{s/=.*/= '$group'/}' /etc/samba/smb.conf 
 
 sed -i '/workgroup/a\        netbios name = '$name'' /etc/samba/smb.conf 
