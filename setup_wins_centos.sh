9737fae9918d1bd09ef97198eebe230dd5a9c90f
GarySunFromChina <36440190+GarySunFromChina@users.noreply.github.com>
2018-08-24 18:32:31 +0800

    Update setup_wins_centos.sh

--- setup_wins_centos.sh
+++ setup_wins_centos.sh
@@ -1,3 +1,11 @@
-yum install -y samba winbind libnss-winbind
+yum install -y samba samba-winbind
+
+systemctl enable smb winbind
+
+#sed -i  '/workgroup/{/wins/!s/$/ wins/}' /etc/nsswitch.conf
 
 sed -i  '/^hosts:/{/wins/!s/$/ wins/}' /etc/nsswitch.conf
+
+
+firewall-cmd --permanent --add-service=samba
+
