ddba54ed195e7305715fc0c5dfaf724d044544d4
GarySunFromChina <36440190+GarySunFromChina@users.noreply.github.com>
2018-08-24 20:06:52 +0800

    Update setup_wins_centos.sh

--- setup_wins_centos.sh
+++ setup_wins_centos.sh
@@ -6,9 +6,9 @@ systemctl enable smb nmb winbind
 group=$1
 namd=$2
 
-sed -i '/workgroup/{/wins/!s/=.*/= \$group/p}' /etc/samba/smb.conf 
+sed -i '/workgroup/{s/=.*/= '$group'/}' /etc/samba/smb.conf 
 
-sed -i '/workgroup/a\        netbios name = $name' /etc/samba/smb.conf 
+sed -i '/workgroup/a\        netbios name = '$name'' /etc/samba/smb.conf 
 
 sed -i '/workgroup/a\        min procotol = SMB2' /etc/samba/smb.conf 
 
